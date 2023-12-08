95e78fd1274cee4870bf7f66ca27b2b6ca128e85
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 08:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+      let dropDown = document.querySelector("#colorSelect");
+      let removeBtn = document.querySelector("input[type='button']");
+
+      let selectedValue;
+
+      dropDown.addEventListener("change", (event) => {
+        selectedValue = event.target.value;
+        console.log(`Selected option: ${selectedValue}`);
+        // console.log(dropDown);
+      });
+
+      removeBtn.addEventListener("click", () => {
+        let options = dropDown.children;
+
+        Array.from(options).forEach((option) => {
+          if (option.value == selectedValue) {
+            option.remove();
+          }
+        });
+        console.dir(dropDown);
+      });
